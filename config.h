6564812cdc71320b1f94d657536a6d134474b783
Bartek Telec <bartektelec@gmail.com>
2023-12-30 02:21:58 +0100

    update layers and mod keys for master side

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
+// #define USE_MATRIX_I2C
 
 /* Select hand configuration */
 
@@ -28,29 +28,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
+// #define QUICK_TAP_TERM 0
+// #define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_SAT_STEP 17
+#    define RGBLIGHT_VAL_STEP 17
 #endif
-
 
 #define OLED_UPDATE_INTERVAL 50
 #define SPLIT_WPM_ENABLE
 #define SPLIT_OLED_ENABLE
 
 #define OCEAN_DREAM_ENABLE
+#define UNICODE_ENABLE
+#define UNICODEMAP_ENABLE
+#define UCIS_ENABLE
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE, UNICODE_MODE_MACOS
+#define OLED_FONT_H "./oledfont.c"
--- keymap.c
+++ keymap.c
@@ -17,13 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 #include <stdio.h>
-enum layers {
-    _QWERTY = 0,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
-};
+#include "process_unicode_common.h"
+enum layers { _QWERTY = 0, _LOWER, _RAISE, _ADJUST, _MOUSE };
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -64,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, XXXXXXX,             KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, AG_TOGG,             KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MEDIA_PLAY_PAUSE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -74,6 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+// clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
@@ -81,7 +79,240 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 
+
 #ifdef OLED_ENABLE
+
+static void render_gui_alt(uint8_t const gui, uint8_t const alt) {
+    static char const gui_off_1[] PROGMEM = {0x85, 0x86, 0};
+    static char const gui_off_2[] PROGMEM = {0xa5, 0xa6, 0};
+    static char const gui_on_1[]  PROGMEM = {0x8d, 0x8e, 0};
+    static char const gui_on_2[]  PROGMEM = {0xad, 0xae, 0};
+
+    static char const alt_off_1[] PROGMEM = {0x87, 0x88, 0};
+    static char const alt_off_2[] PROGMEM = {0xa7, 0xa8, 0};
+    static char const alt_on_1[]  PROGMEM = {0x8f, 0x90, 0};
+    static char const alt_on_2[]  PROGMEM = {0xaf, 0xb0, 0};
+
+    // Fillers between icon frames
+    static char const off_off_1[] PROGMEM = {0xc5, 0};
+    static char const off_off_2[] PROGMEM = {0xc6, 0};
+    static char const on_off_1[]  PROGMEM = {0xc7, 0};
+    static char const on_off_2[]  PROGMEM = {0xc8, 0};
+    static char const off_on_1[]  PROGMEM = {0xc9, 0};
+    static char const off_on_2[]  PROGMEM = {0xca, 0};
+    static char const on_on_1[]   PROGMEM = {0xcb, 0};
+    static char const on_on_2[]   PROGMEM = {0xcc, 0};
+
+    // Top half with in between fillers
+    oled_write_P(gui ? gui_on_1 : gui_off_1, false);
+    if (gui && alt) oled_write_P(on_on_1,   false);
+    else if (gui)   oled_write_P(on_off_1,  false);
+    else if (alt)   oled_write_P(off_on_1,  false);
+    else            oled_write_P(off_off_1, false);
+    oled_write_P(alt ? alt_on_1 : alt_off_1, false);
+
+    // Bottom half with in between fillers
+    oled_write_P(gui ? gui_on_2 : gui_off_2, false);
+    if (gui && alt) oled_write_P(on_on_2,   false);
+    else if (gui)   oled_write_P(on_off_2,  false);
+    else if (alt)   oled_write_P(off_on_2,  false);
+    else            oled_write_P(off_off_2, false);
+    oled_write_P(alt ? alt_on_2 : alt_off_2, false);
+}
+
+
+static void render_ctrl_shift(uint8_t const ctrl, uint8_t const shift) {
+    static char const ctrl_off_1[] PROGMEM = {0x89, 0x8a, 0};
+    static char const ctrl_off_2[] PROGMEM = {0xa9, 0xaa, 0};
+    static char const ctrl_on_1[]  PROGMEM = {0x91, 0x92, 0};
+    static char const ctrl_on_2[]  PROGMEM = {0xb1, 0xb2, 0};
+
+    static char const shift_off_1[] PROGMEM = {0x8b, 0x8c, 0};
+    static char const shift_off_2[] PROGMEM = {0xab, 0xac, 0};
+    static char const shift_on_1[]  PROGMEM = {0xcd, 0xce, 0};
+    static char const shift_on_2[]  PROGMEM = {0xcf, 0xd0, 0};
+
+    // Fillers between icon frames
+    static char const off_off_1[] PROGMEM = {0xc5, 0};
+    static char const off_off_2[] PROGMEM = {0xc6, 0};
+    static char const on_off_1[]  PROGMEM = {0xc7, 0};
+    static char const on_off_2[]  PROGMEM = {0xc8, 0};
+    static char const off_on_1[]  PROGMEM = {0xc9, 0};
+    static char const off_on_2[]  PROGMEM = {0xca, 0};
+    static char const on_on_1[]   PROGMEM = {0xcb, 0};
+    static char const on_on_2[]   PROGMEM = {0xcc, 0};
+
+    // Top half with in between fillers
+    oled_write_P(ctrl ? ctrl_on_1 : ctrl_off_1, false);
+    if (ctrl && shift) oled_write_P(on_on_1,   false);
+    else if (ctrl)     oled_write_P(on_off_1,  false);
+    else if (shift)    oled_write_P(off_on_1,  false);
+    else               oled_write_P(off_off_1, false);
+    oled_write_P(shift ? shift_on_1 : shift_off_1, false);
+
+    // Bottom half with in between fillers
+    oled_write_P(ctrl ? ctrl_on_2 : ctrl_off_2, false);
+    if (ctrl && shift) oled_write_P(on_on_2,   false);
+    else if (ctrl)     oled_write_P(on_off_2,  false);
+    else if (shift)    oled_write_P(off_on_2,  false);
+    else               oled_write_P(off_off_2, false);
+    oled_write_P(shift ? shift_on_2 : shift_off_2, false);
+}
+
+void render_bootmagic_status(uint8_t col, uint8_t line) {
+    /* Show Ctrl-Gui Swap options */
+    oled_set_cursor(col, line);
+
+    if (keymap_config.swap_lalt_lgui) {
+        oled_write_P(PSTR("OSX"), false);
+    } else {
+        oled_write_P(PSTR("WIN"), false);
+    }
+}
+
+/**
+ * @brief Renders the active layers to the OLED
+ *
+ */
+void render_layer_state(uint8_t col, uint8_t line) {
+    // clang-format off
+    static const char PROGMEM tri_layer_image[][3][24] = {
+        // base
+        {
+            {
+                0x00, 0x00, 0x00, 0x80, 0x80, 0x40,
+                0x40, 0x20, 0x20, 0x10, 0x10, 0x08,
+                0x08, 0x10, 0x10, 0x20, 0x20, 0x40,
+                0x40, 0x80, 0x80, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x88, 0x88, 0x5D,
+                0x5D, 0x3E, 0x3E, 0x7C, 0x7C, 0xF8,
+                0xF8, 0x7C, 0x7C, 0x3E, 0x3E, 0x5D,
+                0x5D, 0x88, 0x88, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+                0x01, 0x02, 0x02, 0x04, 0x04, 0x08,
+                0x08, 0x04, 0x04, 0x02, 0x02, 0x01,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+            }
+        },
+        // raise
+        {
+            {
+                0x00, 0x00, 0x00, 0x80, 0x80, 0xC0,
+                0xC0, 0xE0, 0xE0, 0xF0, 0xF0, 0xF8,
+                0xF8, 0xF0, 0xF0, 0xE0, 0xE0, 0xC0,
+                0xC0, 0x80, 0x80, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x88, 0x88, 0x55,
+                0x55, 0x23, 0x23, 0x47, 0x47, 0x8F,
+                0x8F, 0x47, 0x47, 0x23, 0x23, 0x55,
+                0x55, 0x88, 0x88, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+                0x01, 0x02, 0x02, 0x04, 0x04, 0x08,
+                0x08, 0x04, 0x04, 0x02, 0x02, 0x01,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+            }
+        },
+        // lower
+        {
+            {
+                0x00, 0x00, 0x00, 0x80, 0x80, 0x40,
+                0x40, 0x20, 0x20, 0x10, 0x10, 0x08,
+                0x08, 0x10, 0x10, 0x20, 0x20, 0x40,
+                0x40, 0x80, 0x80, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x88, 0x88, 0xD5,
+                0xD5, 0xE2, 0xE2, 0xC4, 0xC4, 0x88,
+                0x88, 0xC4, 0xC4, 0xE2, 0xE2, 0xD5,
+                0xD5, 0x88, 0x88, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+                0x01, 0x03, 0x03, 0x07, 0x07, 0x0F,
+                0x0F, 0x07, 0x07, 0x03, 0x03, 0x01,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+            }
+        },
+        // adjust
+        {
+            {
+                0x00, 0x00, 0x00, 0x80, 0x80, 0x40,
+                0xC0, 0x60, 0xA0, 0x50, 0xB0, 0x58,
+                0xA8, 0x50, 0xB0, 0x60, 0xA0, 0x40,
+                0xC0, 0x80, 0x80, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x88, 0x88, 0x5D,
+                0xD5, 0x6B, 0xB6, 0x6D, 0xD6, 0xAD,
+                0xDA, 0x6D, 0xD6, 0x6B, 0xB6, 0x5D,
+                0xD5, 0x88, 0x88, 0x00, 0x00, 0x00
+            },
+            {
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+                0x01, 0x03, 0x02, 0x05, 0x06, 0x0D,
+                0x0A, 0x05, 0x06, 0x03, 0x02, 0x01,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+            }
+        },
+        // blank
+        {
+            { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }
+        },
+        // mouse
+        {
+            { 0,  0,  0,  0,  0,  0,  0,  0,192, 32, 32, 32,160, 32, 32, 32,192,  0,  0,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,240, 15,  0,  0,  0,  3,  0,  0,  0, 15,240,  0,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,  3,  6,  4,  4,  4,  4,  4,  4,  4,  6,  3,  0,  0,  0,  0,  0,  0 }
+        }
+    };
+
+  #define L_BASE 0
+  #define L_RAISE 1
+  #define L_LOWER 2
+  #define L_ADJUST 3
+  #define L_BLANK 4
+  #define L_MOUSE 5
+
+    // clang-format on
+    uint8_t layer_is[4] = {L_BASE, L_BLANK};
+    if (layer_state_is(_ADJUST)) {
+        layer_is[0] = L_ADJUST;
+    } else if (layer_state_is(_RAISE)) {
+        layer_is[0] = L_RAISE;
+    } else if (layer_state_is(_LOWER)) {
+        layer_is[0] = L_LOWER;
+    }
+
+    if (layer_state_is(_MOUSE)) {
+        layer_is[1] = L_MOUSE;
+    }
+
+    oled_set_cursor(col, line);
+    oled_write_raw_P(tri_layer_image[layer_is[0]][0], sizeof(tri_layer_image[0][0]));
+
+    oled_set_cursor(col, line + 1);
+    oled_write_raw_P(tri_layer_image[layer_is[0]][1], sizeof(tri_layer_image[0][0]));
+
+    oled_set_cursor(col, line + 2);
+    oled_write_raw_P(tri_layer_image[layer_is[0]][2], sizeof(tri_layer_image[0][0]));
+
+    // oled_set_cursor(col, line + 3);
+    // oled_write_raw_P(tri_layer_image[layer_is[1]][0], sizeof(tri_layer_image[0][0]));
+    // oled_set_cursor(col, line + 4);
+    // oled_write_raw_P(tri_layer_image[layer_is[1]][1], sizeof(tri_layer_image[0][0]));
+    // oled_set_cursor(col, line + 5);
+    // oled_write_raw_P(tri_layer_image[layer_is[1]][2], sizeof(tri_layer_image[0][0]));
+}
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
@@ -91,69 +322,26 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 void render_crkbd_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
+    static const char PROGMEM crkbd_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
     oled_write_P(crkbd_logo, false);
 }
 
 
-void render_default_layer_state(void) {
-    oled_write_P(PSTR("Lyout"), false);
-    switch (get_highest_layer(default_layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR(" QRTY"), false);
-            break;
-    }
-}
-
-void render_layer_state(void) {
-    oled_write_P(PSTR("LAYER"), false);
-    oled_write_P(PSTR("Lower"), layer_state_is(_LOWER));
-    oled_write_P(PSTR("Raise"), layer_state_is(_RAISE));
-}
-
-
-void render_mod_status(uint8_t modifiers) {
-    oled_write_P(PSTR("Mods:"), false);
-    oled_write_P(PSTR(" "), false);
-    oled_write_P(PSTR("S"), (modifiers & MOD_MASK_SHIFT));
-    oled_write_P(PSTR("C"), (modifiers & MOD_MASK_CTRL));
-    oled_write_P(PSTR("A"), (modifiers & MOD_MASK_ALT));
-    oled_write_P(PSTR("G"), (modifiers & MOD_MASK_GUI));
-}
-
-void render_bootmagic_status(void) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    oled_write_P(PSTR("BTMGK"), false);
-    oled_write_P(PSTR(" "), false);
-    oled_write_P(logo[0][0], !keymap_config.swap_lctl_lgui);
-    oled_write_P(logo[1][0], keymap_config.swap_lctl_lgui);
-    oled_write_P(PSTR(" "), false);
-    oled_write_P(logo[0][1], !keymap_config.swap_lctl_lgui);
-    oled_write_P(logo[1][1], keymap_config.swap_lctl_lgui);
-    oled_write_P(PSTR(" NKRO"), keymap_config.nkro);
-}
-
 void render_status_main(void) {
     /* Show Keyboard Layout  */
-    render_default_layer_state();
-    render_mod_status(get_mods());
-    render_bootmagic_status();
+    uint8_t mods = get_mods();
+    render_bootmagic_status(0, 1);
+    render_layer_state(0, 4);
+    // render_os();
 
-    render_keylogger_status();
+    oled_set_cursor(0,10);
+    render_gui_alt(mods & MOD_MASK_GUI, mods & MOD_MASK_ALT);
+    render_ctrl_shift(mods & MOD_MASK_CTRL, mods & MOD_MASK_SHIFT || host_keyboard_led_state().caps_lock);
 }
 
 bool oled_task_user(void) {
-    update_log();
     if (is_keyboard_master()) {
-        render_status_main();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+        render_status_main(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
         render_crkbd_logo();
     }
@@ -162,7 +350,6 @@ bool oled_task_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
-        add_keylog(keycode);
     }
     return true;
 }
